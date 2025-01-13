58aa536a7a965c94af1513d1efecd352c318dbec
Sára Wokounová <sara@wokounova.cz>
2025-01-13 20:35:50 +0100

    Added more tests

--- src/test/java/cz/sda/java/remotesk1/invoices/service/ClientServiceBeanTest.java
+++ src/test/java/cz/sda/java/remotesk1/invoices/service/ClientServiceBeanTest.java
@@ -1,7 +1,9 @@
 package cz.sda.java.remotesk1.invoices.service;
 
+import cz.sda.java.remotesk1.invoices.exception.NotFoundException;
 import cz.sda.java.remotesk1.invoices.model.Client;
 import cz.sda.java.remotesk1.invoices.repository.ClientRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
@@ -48,5 +50,32 @@ class ClientServiceBeanTest {
         assert client.getName().equals("Jane Doe");
         assert client.getAddress().equals("USA");
         assert client.getId().equals(uuid);
+    }
+
+    @Test
+    void removeClientShouldPass() {
+        // given
+        var uuid = UUID.randomUUID().toString();
+        Mockito.when(clientRepository.existsById(Mockito.any(String.class)))
+                .thenAnswer(invocation -> true);
+        // when
+        clientServiceBean.removeClient(uuid);
+        // then
+        Mockito.verify(clientRepository, Mockito.times(1)).deleteById(Mockito.any(String.class));
+    }
+
+    @Test
+    void removeClientShouldThrowNotFoundWhenClientDoesntExist() {
+        // given
+        var uuid = UUID.randomUUID().toString();
+        Mockito.when(clientRepository.existsById(Mockito.any(String.class)))
+                .thenAnswer(invocation -> false);
+        // when
+        Exception exception = Assertions.assertThrows(
+                NotFoundException.class,
+                () -> clientServiceBean.removeClient(uuid) // Code that should throw
+        );
+
+        Assertions.assertEquals("Client with id " + uuid + " does not exist", exception.getMessage());
     }
 }
